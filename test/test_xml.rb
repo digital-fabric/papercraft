5f68567876f6984127610bc2887298f347684362
Sharon Rosner <sharon@noteflakes.com>
2025-09-11 10:23:43 +0200

    Fix XML test

--- test/test_xml.rb
+++ test/test_xml.rb
@@ -11,13 +11,13 @@ class HtmlTest < Minitest::Test
       {
         title:  'foo',
         url:    '/01-foo',
-        date:   '2025-01-01',
+        date:   'Tue, 01 Jan 2025',
         markdown: '# Foo & Bar'
       },
       {
         title:  'bar',
         url:    '/02-bar',
-        date:   '2025-02-02',
+        date:   'Sat, 02 Feb 2025',
         markdown: '# Bar & Baz'
       },
     ]
@@ -37,7 +37,7 @@ class HtmlTest < Minitest::Test
               title e[:title]
               link "https://noteflakes.com#{e[:url]}"
               guid "https://noteflakes.com#{e[:url]}"
-              pubDate Time.parse(e[:date]).httpdate
+              pubDate "#{e[:date]} 00:00:00 GMT"
               description P2.markdown(e[:markdown]).chomp
             }
           }
@@ -45,7 +45,7 @@ class HtmlTest < Minitest::Test
       }
     }
 
-    expected = '<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom"><channel><title>Foo</title><link>https://foo.com/</link><description>Foo RSS</description><language>en-us</language><pubDate>Thu, 11 Sep 2025 08:00:00 GMT</pubDate><atom:link href="https://noteflakes.com/feeds/rss" rel="self" type="application/rss+xml" /> <item><title>foo</title><link>https://noteflakes.com/01-foo</link><guid>https://noteflakes.com/01-foo</guid><pubDate>Tue, 31 Dec 2024 23:00:00 GMT</pubDate><description>&lt;h1 id=&quot;foo--bar&quot;&gt;Foo &amp;amp; Bar&lt;/h1&gt;</description></item><item><title>bar</title><link>https://noteflakes.com/02-bar</link><guid>https://noteflakes.com/02-bar</guid><pubDate>Sat, 01 Feb 2025 23:00:00 GMT</pubDate><description>&lt;h1 id=&quot;bar--baz&quot;&gt;Bar &amp;amp; Baz&lt;/h1&gt;</description></item></channel></rss>'
+    expected = '<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom"><channel><title>Foo</title><link>https://foo.com/</link><description>Foo RSS</description><language>en-us</language><pubDate>Thu, 11 Sep 2025 08:00:00 GMT</pubDate><atom:link href="https://noteflakes.com/feeds/rss" rel="self" type="application/rss+xml" /> <item><title>foo</title><link>https://noteflakes.com/01-foo</link><guid>https://noteflakes.com/01-foo</guid><pubDate>Tue, 01 Jan 2025 00:00:00 GMT</pubDate><description>&lt;h1 id=&quot;foo--bar&quot;&gt;Foo &amp;amp; Bar&lt;/h1&gt;</description></item><item><title>bar</title><link>https://noteflakes.com/02-bar</link><guid>https://noteflakes.com/02-bar</guid><pubDate>Sat, 02 Feb 2025 00:00:00 GMT</pubDate><description>&lt;h1 id=&quot;bar--baz&quot;&gt;Bar &amp;amp; Baz&lt;/h1&gt;</description></item></channel></rss>'
     assert_equal expected, t.render_xml
   end
 end
